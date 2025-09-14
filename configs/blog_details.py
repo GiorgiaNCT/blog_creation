e304b7b07c4e869183fb2ab85fd7d3b707f86555
GiorgiaNCT <giorgia.carranza2@gmail.com>
2025-09-14 22:17:29 +0200

    add another page gnerated

---
+++ configs/blog_details.py
@@ -0,0 +1,14 @@
+blog_post = {
+    'cdmx_dia_de_los_muertos_2024.html': {
+        'title':'CDMX Celebrates Día de los Muertos 2024',
+        'title_blog': 'CDMX Celebrates Día de los Muertos 2024',
+        'subtitle_post': 'Dia del los muerto parade',
+        'tags': ['Photo'],
+        'text_blog':"""
+                    <p>Mexico City during Día de los Muertos is nothing short of magical. The streets come alive with vibrant colors, intricate altars, and the haunting beauty of Catrinas—those elegant skeletal figures that have become synonymous with this sacred celebration of life and death.</p>
+                    <img src="https://raw.githubusercontent.com/GiorgiaNCT/photo_blog/c7d77ee569e8cec6157c55ece5012372cb3bb1c3/6-DSCF5719.jpg" alt="Woman with flower" class="gallery-image">
+                    <img src="https://raw.githubusercontent.com/GiorgiaNCT/photo_blog/9243c3184388fd8b4e9cc18c9409fbe6a1bedd35/2-DSCF5769.jpg" alt="Mask " class="gallery-image">
+
+"""
+    }
+}
