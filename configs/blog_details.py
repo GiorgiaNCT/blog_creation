af670fdbaf30ce73af63671873692d6e468f77da
GiorgiaNCT <giorgia.carranza2@gmail.com>
2025-09-15 12:10:10 +0200

    try new config

--- configs/blog_details.py
+++ configs/blog_details.py
@@ -1,4 +1,7 @@
-blog_post = {
+from configs.post_class import Post
+from datetime import date
+
+blog_post_old = {
     'cdmx_dia_de_los_muertos_2024.html': {
         'title':'CDMX Celebrates Día de los Muertos 2024',
         'title_blog': 'CDMX Celebrates Día de los Muertos 2024',
@@ -12,3 +15,25 @@ blog_post = {
 """
     }
 }
+
+blog_post = [
+    Post(
+        filename='cdmx_dia_de_los_muertos_2024_new.html',
+        title='CDMX Celebrates Día de los Muertos 2024',
+        blog_title='CDMX Celebrates Día de los Muertos 2024',
+        blog_subtitle='Dia del los muerto parade',
+        blog_content="""
+                    <p>Mexico City during Día de los Muertos is nothing short of magical. The streets come alive with vibrant colors, intricate altars, and the haunting beauty of Catrinas—those elegant skeletal figures that have become synonymous with this sacred celebration of life and death.</p>
+                    <img src="https://raw.githubusercontent.com/GiorgiaNCT/photo_blog/c7d77ee569e8cec6157c55ece5012372cb3bb1c3/6-DSCF5719.jpg" alt="Woman with flower" class="featured-image">
+                    <img src="https://raw.githubusercontent.com/GiorgiaNCT/photo_blog/9243c3184388fd8b4e9cc18c9409fbe6a1bedd35/2-DSCF5769.jpg" alt="Mask " class="featured-image">
+        """,
+        preview="""
+                    <img src="https://raw.githubusercontent.com/GiorgiaNCT/photo_blog/c7d77ee569e8cec6157c55ece5012372cb3bb1c3/6-DSCF5719.jpg" alt="Tokyo street in rain" class="post-image">
+                    <p class="post-excerpt">
+                        Last year I experienced the vibrant Día de los Muertos parade in Mexico, where music, colors, and tradition filled the streets. It was a powerful celebration of life, memory, and culture.
+                    </p>
+        """,
+        tags=['Photo'],
+        date=date(2025, 9, 15)
+    )
+]
